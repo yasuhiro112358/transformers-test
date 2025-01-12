d0a7d6733e382c281a04e48beba544fc8a8d2ff6
Yasuhiro WATANABE <info@nabeyasu.com>
2025-01-13 00:25:40 +0900

    feat: refactor OpenAI API example script with additional test functions

--- src/examples/openai_api.py
+++ src/examples/openai_api.py
@@ -1,17 +1,10 @@
-from dotenv import load_dotenv
 import os
+import asyncio
+from dotenv import load_dotenv
 from openai import OpenAI
 
-def main():
-    # Load environment variables
-    load_dotenv()
-
-    # Create an OpenAI client
-    client = OpenAI(
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    )
-
-    messages = [
+def test_9_3(client: OpenAI) -> None:
+    messages: list[dict[str, str]] = [
         {
             "role": "user",
             "content": "日本で一番高い山は何？"
@@ -28,6 +21,67 @@ def main():
     )
 
     print(chat_completion.choices[0].message.content)
+
+def test_9_5(client: OpenAI) -> None:
+    messages: list[dict[str, str]] = [
+        {
+            "role": "user",
+            "content": "数字を１から１０まで読み上げてください"
+        },
+    ]
+
+    completion = client.chat.completions.create(
+        # model="gpt-4o",
+        model="gpt-4o-mini",
+        # model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.0,
+        # 生成するトークンの最大数を指定する
+        # max_tokens=1,
+        # max_tokens=3,
+        max_tokens=10,
+    )
+
+    print(completion.choices[0].message.content)
+
+def test_9_6(client: OpenAI) -> None:
+    messages: list[dict[str, str]] = [
+        {
+            "role": "user",
+            "content": "数字を1から10まで読み上げてください"
+        },
+    ]
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        temperature=1.0,
+        # 終端文字を指定する
+        stop="5",
+    )
+
+    print(completion.choices[0].message.content)
+
+def test_9_7(client: OpenAI) -> None:
+    pass
+
+
+
+def main() -> None:
+    # Load environment variables
+    load_dotenv()
+
+    # Create an OpenAI client
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    # Test the API
+    # test_9_3(client)
+    # test_9_5(client)
+    # test_9_6(client)
+    test_9_7(client)
+
 
 
 if __name__ == "__main__":
